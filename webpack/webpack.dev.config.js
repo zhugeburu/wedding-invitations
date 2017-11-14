04952c5f988fefecc80ada9917db7cbbdd3a8301
陈钦辉 <chenqinhui@didichuxing.com>
2017-11-14 19:38:44 +0800

    @dev update

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -23,7 +23,7 @@ module.exports = {
     output: {
         path: DIST_PATH,
         filename: 'bundle.js',
-        publicPath: 'http://127.0.0.1:8081/'
+        publicPath: 'http://172.23.164.43:8081/'
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
