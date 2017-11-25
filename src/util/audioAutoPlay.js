9c3f2b3bf09180011efa7932383d9e43216068e6
陈钦辉 <chenqinhui@didichuxing.com>
2017-11-25 22:00:41 +0800

    @dev udpate

--- src/util/audioAutoPlay.js
+++ src/util/audioAutoPlay.js
@@ -16,6 +16,15 @@ export function autoPlay(eId) {
       });
       wx.ready(()=> {
         document.getElementById(eId).play();
+        var shareConfig = {
+          title: '1111',
+          desc: '2222',
+          link: 'http://www.if-elseif-else.com/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: 'http://qnm.hunliji.com/o_1bniiv6lv1lnf1rcoa6u1ipfueul.jpg?imageView2/1/w/200/h/200'
+        }
+        wx.onMenuShareAppMessage(shareConfig);
+        wx.onMenuShareTimeline(shareConfig);
+
       });
     }
 }
