45a24b9407e2e3f8c2772baae74c9373f1e9999a
陈钦辉 <chenqinhui@didichuxing.com>
2017-12-02 22:27:20 +0800

    @dev done

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -4,7 +4,7 @@
 import React, {Component} from 'react';
 import './Loading.scss';
 
-const loadingImg = require('../../asset/images/loading.svg');
+const loadingImg = require('../../asset/images/loading-old.svg');
 
 export default class Loading extends Component {
     render() {
